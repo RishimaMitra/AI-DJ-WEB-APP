afc876ac91f1427694b20a916799da5ad9604a89
RishimaMitra <100062259+RishimaMitra@users.noreply.github.com>
2022-08-20 16:24:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+song="";
+function preload(){
+song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+    
+    //poseNet = ml5.poseNet(video , modelLoaded);
+    //poseNet.on(video , gotPoses);
+}
+
+function draw(){
+    image(video,  00 ,0, 600,500);
+    fill("#FF0000");
+	stroke("#FF0000");
+
+}
+
+ function modelLoaded(){
+
+}
+function play(){
+    song.play();
+}
